5b1724628f9eb664044a39c6c5ed3d67bf4183f1
kgarcia7 <kgarcia7814@gmail.com>
2023-11-12 19:18:11 -0500

    Update htmlRoute file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const apiRoutes = require ('./routes/apiRoutes');
+const htmlRoutes = require ('./routes/htmlRoutes')
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express(); 
+
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.static('public'));
+app.use(express.urlencoded({  extended: true  }));
+app.use(express.json());
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
