50002e6df4620eafaf8956205b4c15b2cc856833
kgarcia7 <kgarcia7814@gmail.com>
2023-11-15 17:06:49 -0500

    updating server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 //Middleware for parsing JSON and urlencoded form data
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({  extended: true  }));
+app.use(express.json());
 
 
 app.listen(PORT, () =>
