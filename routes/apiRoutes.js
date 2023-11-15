17718e842895d04ee3f2c892c186d44da6c586de
kgarcia7 <kgarcia7814@gmail.com>
2023-11-15 17:21:08 -0500

    Update apiRoutes.js syntax and comments, added delete request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,37 @@
 const path = require('path');
 const fs = require('fs');
 
-//GET request to read db.json and return all notes as json
+// unique id npm package 
+var uniqid = require('uniqid')
+
 module.exports = (app) => {
+    
+    //GET request to read db.json and return all notes as json
+    app.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
 
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-});
+    //POST request to update and save new note 
+    app.post('/api/notes', (req,res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uniqid(),
+        };
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
 
-//POST request to update and save new note 
-app.post('/api/notes', (req,res) => {
-    let db = fs.readFileSync('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
-    let userNote = {
-        title: req.body.title,
-        text: req.body.text,
-    };
-    db.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
-})
+    //Delete request 
+    app.delete('/api/notes/:id', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'));
+        let deleteNote = db.filter(item => item.id !== req.params.id);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
+        res.json(deleteNote);
+    })
+
 };
