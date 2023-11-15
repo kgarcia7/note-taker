a5cdb8437f517e6bfbd2ecbd29cc5661ee217010
kgarcia7 <kgarcia7814@gmail.com>
2023-11-15 17:12:27 -0500

    update comments for middleware in server.js

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express(); 
 
-//Middleware for parsing JSON and urlencoded form data
+//Middleware for serving static files (public directory)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware for parsing urlencoded form data
 app.use(express.urlencoded({  extended: true  }));
+
+//Middleware for parsing JSON data
 app.use(express.json());
 
 
